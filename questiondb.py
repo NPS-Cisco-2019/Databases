11717eb3c4d96f5e768f2d10720ac1bab1e2f3ff
NPS-Cisco-2019 <54304816+NPS-Cisco-2019@users.noreply.github.com>
2019-11-28 12:28:36 +0530

    Update questiondb.py

--- questiondb.py
+++ questiondb.py
@@ -11,12 +11,34 @@ def addquestion(question, answer):
     c = conn.cursor()
     insertion = [str(question), str(answer)]
     
-    c.executemany("INSERT INTO questions VALUES (?,?)",insertion)
+    c.execute("INSERT INTO questions VALUES (?,?)",insertion)
+    conn.commit()
+    conn.close()
+
+def removequestion(question):
+    conn = sqlite3.connect('requests.db')
+    c = conn.cursor()
+    c.execute("DELETE FROM questions WHERE qjson = (?)",(str(question),))
     conn.commit()
     conn.close()
     
-def run_request():
+def run_request(question, current_dict):
     conn = sqlite3.connect('requests.db')
     c = conn.cursor()
     for row in c.execute("SELECT * FROM questions"):
+        if row[0] == str(question):
+            if row[1]["success"]:
+        
+                current_dict["question"] = question["question"]
+                current_dict["answers"] = row[1]["answer"]
+                current_dict["websites"] = row[1]["domain"]
+                removequestion(question)
+
+            else:
+                current_dict["question"] = question["question"]
+                current_dict["answers"] = "ERROR"
+                current_dict["websites"] = "NOT FOUND"
+
+        
+
         
